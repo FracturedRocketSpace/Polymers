eaf3152487f0c1cf50b184762d9905b1140d7fe7
mgarming <mgarming@outlook.com>
2016-03-09 11:15:04 +0100

    Clean up

--- plotPolymers.py
+++ plotPolymers.py
@@ -2,10 +2,9 @@
 # Plot polymer
 #==============================================================================
 
-import numpy as np
-import config as c
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
 
 def plotPolymers(polymers):
     plt.figure(1)
