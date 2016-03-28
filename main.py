f40e7018633d5b5c1b1cb8469ec5197869fbca42
mgarming <mgarming@outlook.com>
2016-03-28 19:26:16 +0200

    use polweight

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from addPolymers import addPolymers
 from plotPolymers import plotPolymers
 from minimizeEp import minimizeEp
 import matplotlib.pyplot as plt
+import math
 
 
 # Generate the polymers
@@ -74,17 +75,27 @@ print ("Gyradius squared of each polymer:", gyradius)
 print ("Average gyradius squared=", averageGyradiusSquared, "with standard deviation=", errorAverageGyradiusSquared)
 
 # Calculate and plot pesistance length
-lp1=np.zeros([c.nBeads, len(polymers) ])
-lp2=np.zeros([c.nBeads, len(polymers) ])
+lp1=np.zeros([len(polymers),1])
+totalWeight=np.zeros([len(polymers),1])
 for l in range(len(polymers)):
+    lp1local=np.zeros([np.size(polymers[l]-1,0), 1 ])
     for k in range(np.size(polymers[l],0)-1):
         a=polymers[l][k+1,:]-polymers[l][k,:]
-        lp1[k,l] = np.dot(a, polymers[l][-1,:]-polymers[l][k,:]  ) / c.linkDistance # Definition from 2 papers
-#        lp2[k,l] = np.dot(a, polymers[l][-1,:] ) / c.linkDistance # Definition from 1 paper
+        lp1local[k] = np.dot(a, polymers[l][-1,:]-polymers[l][k,:]  ) / c.linkDistance
+    lp1[l]=np.dot( polWeights[l].T , lp1local ) / np.sum(polWeights[l])
+    totalWeight[l]=np.sum(polWeights[l])
 
+lp1Avg=np.dot( totalWeight.T , lp1 ) / np.sum(totalWeight);   # Take average over polymers
+# Calculate error persistence length
+nBlocks=10;
+lBlock=len(polymers)/nBlocks;
+lpBlocks=np.zeros([nBlocks,1])
+for j in range(nBlocks):
+    lpBlocks[j]=np.dot( totalWeight[math.floor(j*lBlock) : math.floor( (j+1)*lBlock)].T , lp1[math.floor(j*lBlock):math.floor( (j+1)*lBlock)] ) / np.sum(totalWeight[math.floor(j*lBlock):math.floor( (j+1)*lBlock)])
+
+lpError=math.sqrt( (np.mean(lpBlocks*lpBlocks)- np.mean(lpBlocks)**2) / nBlocks )
 plt.figure(2)
-plt.plot(np.mean(lp1,1))
-#plt.plot(np.mean(lp2,1))
-#plt.legend(["lp1", "lp2"])
-print ("Average persistence length method 1: ",np.mean(lp1), "std: ", np.std(np.mean(lp1,1) ))
-#print ("Average persistence length method 2: ",np.mean(lp2), "std: ", np.std(np.mean(lp2,1) ))
+plt.plot(lp1)
+plt.xlabel('Polymer number')
+plt.ylabel('Average local persistence length')
+print ("Average persistence length method 1: ", lp1Avg, "Error: ", lpError)
